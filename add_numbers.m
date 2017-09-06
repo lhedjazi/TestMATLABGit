1c1683d677017f14eb5fca942a54dcafa7c15d8c
lhedjazi <lhedjazi@users.noreply.github.com>
2017-09-06 12:00:26 +0200

    Add files via upload

---
+++ add_numbers.m
@@ -0,0 +1,10 @@
+function [c] = add_numbers(a,b) 
+% ADD_NUMBERS Simple demo for demonstrating FW and Git integration
+g
+% Auth/Revision:  Arvind Hosagrahara 
+%                 Copyright 2016 The MathWorks Consulting Group 
+%                 $Id$
+
+c = a+b+5;
+% end
+end %function
---
+++ add_numbers2.m
@@ -0,0 +1,10 @@
+function [c] = add_numbers2(a,b) 
+% ADD_NUMBERS2 Simple demo for demonstrating FW and Git integration
+g
+% Auth/Revision:  AH 
+%                 Copyright 2016 The MathWorks AEG Group 
+%                 $Id$
+
+c = a+b+6;
+
+end %function
